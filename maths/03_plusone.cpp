5707da12a16bc663cea1a918d547d30b9b30e6bc
alwaysalazycoder <gs314315316@gmail.com>
2023-01-29 23:13:17 +0530

    Normal change

--- maths/03_plusone.cpp
+++ maths/03_plusone.cpp
@@ -1,32 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-int n ;
-cin >> n ;
+    int n;
+    cin >> n;
 
-int arr[n];
-for(int i = 0 ; i < n ; ++i)
-{
-    cin >> arr[i];
-}
-
-int last = arr[n-1];
-for(int i = n-1 ; i >= 0 ; ++i)
-{
-    if(arr[i] == 9)
+    int arr[n];
+    for (int i = 0; i < n; ++i)
     {
-        arr[i] = 0 ;
+        cin >> arr[i];
     }
 
-    else
+    int last = arr[n - 1];
+    for (int i = n - 1; i >= 0; ++i)
     {
-        arr[i] = arr[i] + 1 ;
+        if (arr[i] == 9)
+        {
+            arr[i] = 0;
+        }
+
+        else
+        {
+            arr[i] = arr[i] + 1;
+        }
     }
-}
 
     arr.push_back(0);
-      arr[0] = 1;
-      cout << digit;
-
+    arr[0] = 1;
+    cout << digit;
 }
